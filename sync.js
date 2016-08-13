c790a0f7517093f114ba97c268c4ab2e0c0e674b
popomore <sakura9515@gmail.com>
2016-08-14 02:24:07 +0800

    refactor: use new version of github-labels

--- sync.js
+++ sync.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
 const cp = require('child_process');
 
 // Get the access token that is encrypted by travis
 const token = getToken();
-fs.writeFileSync(path.join(__dirname, '.github-labels'), token);
 
 // Spawn github-labels
 const githubLabels = path.join(__dirname, 'node_modules/.bin/labels');
-const args = [ '-c', path.join(__dirname, 'labels.json'), 'egg/egg-labels' ];
+const args = [
+  '-c', path.join(__dirname, 'labels.json'), 'eggjs/egg-labels',
+  '-t', token,
+];
 const env = Object.assign({}, process.env, { HOME: __dirname });
 cp.fork(githubLabels, args, { env })
 .on('exit', code => process.exit(code));
